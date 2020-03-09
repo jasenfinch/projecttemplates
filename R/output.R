945e54809caabc29002377c48ef9ec895809b275
jasenfinch <jsf9@aber.ac.uk>
2020-03-09 17:51:35 +0000

    added report and presenation templates

--- R/output.R
+++ R/output.R
@@ -1,12 +1,58 @@
+#' @importFrom lubridate month year
 
 output <-  function(project_name,project_directory,type){
   path <- str_c(project_directory,'/',type)
   
   dir.create(path)
   
+  current_date <- Sys.Date()
+  current_month <- current_date %>% 
+    month(label = TRUE,abbr = FALSE) %>% 
+    as.character()
+  current_year <- current_date %>%
+    year()
+  
   templates <- list(
-    report = ' ',
-    presentation = ' ',
+    report = glue('---
+title: "{project_name}"
+output: html_document
+bibliography: references.bib
+---
+
+```{{r setup, include=FALSE}}
+knitr::opts_chunk$set(echo = FALSE,message = FALSE,warning = FALSE)
+```
+'),
+    presentation = glue('---
+output:
+  xaringan::moon_reader:
+    seal: false
+    lib_dir: libs
+    css: ["default","default-fonts", "custom-theme.css"]
+    nature:
+      highlightStyle: github
+      highlightLines: true
+      countIncrementalSlides: false
+      self_contained: true
+---
+
+```{{r setup, include=FALSE}}
+opts_chunk$set(echo = FALSE,dpi = 300,message = FALSE)
+options(htmltools.dir.version = FALSE)
+```
+
+class: inverse, center, middle, title-slide
+
+# {project_name}
+
+<br>
+
+### {{Author names}}
+
+### {{Event name}}
+
+#### {current_month} {current_year}
+'),
     manuscript = glue('---
 output: 
   bookdown::word_document2:
@@ -131,9 +177,16 @@ knitr::opts_chunk$set(echo = FALSE,
   
   template_directory <- system.file('templates',package = 'projecttemplates')
   
-  if (type != 'manuscript') {
+  if (type == 'report') {
     writeLines(templates[[type]],str_c(path,'/',type,'.Rmd')) 
-  } else {
+  } 
+  
+  if (type == 'presentation') {
+    invisible(file.copy(str_c(template_directory,'custom-theme.css',sep = '/'),str_c(project_directory,type,sep = '/')))
+    writeLines(templates[[type]],str_c(path,'/',type,'.Rmd')) 
+  }
+  
+  if (type == 'manuscript') {
     invisible(file.copy(str_c(template_directory,'template.docx',sep = '/'),str_c(project_directory,type,sep = '/')))
     invisible(file.copy(str_c(template_directory,'manuscriptCheck.R',sep = '/'),str_c(project_directory,'R',sep = '/')))
     writeLines(templates[[type]],str_c(path,'/',type,'.Rmd'))
