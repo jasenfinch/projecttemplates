5fa17c10c2ad0fa9d3150cfda8c27a9b120bd2f5
jasenfinch <jsf9@aber.ac.uk>
2020-06-25 00:21:14 +0100

    test fix

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -1,16 +1,37 @@
 
 context('template')
 
-test_that('template works',{
+test_that('report template works',{
   temp_path <- tempdir()
   
   template('report test',path = temp_path,type = 'report',start = FALSE)
-  template('manuscript test',path = temp_path,type = 'manuscript',start = FALSE)
-  template('presentation test',path = temp_path,type = 'presentation',start = FALSE)
   
   temp_dirs <- list.dirs(temp_path,recursive = FALSE)
   
   expect_true(TRUE %in% stringr::str_detect(temp_dirs,'report_test'))
-  expect_true(TRUE %in% stringr::str_detect(temp_dirs,'manuscript_test'))
+})
+
+test_that('presentation template works',{
+  temp_path <- tempdir()
+  
+  template('presentation test',path = temp_path,type = 'presentation',start = FALSE)
+  
+  temp_dirs <- list.dirs(temp_path,recursive = FALSE)
+  
   expect_true(TRUE %in% stringr::str_detect(temp_dirs,'presentation_test'))
+})
+
+test_that('manuscript template works',{
+  
+  if (!('wordcountaddin' %in% rownames(installed.packages()))){
+    devtools::install_github('benmarwick/wordcountaddin')
+  }
+  
+  temp_path <- tempdir()
+  
+  template('manuscript test',path = temp_path,type = 'manuscript',start = FALSE)
+  
+  temp_dirs <- list.dirs(temp_path,recursive = FALSE)
+  
+  expect_true(TRUE %in% stringr::str_detect(temp_dirs,'manuscript_test'))
 })
