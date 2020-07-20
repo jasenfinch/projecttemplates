ed7c1a7f300b90f49c50ab46bd025179f2b6ddeb
jasenfinch <jsf9@aber.ac.uk>
2020-07-20 11:38:31 +0100

    .drake directory now ignored

--- R/git.R
+++ R/git.R
@@ -3,8 +3,9 @@
 createGit <- function(project_directory){
   message('Initialising git')
   
+  ignore <- c('.Rproj.user','.drake')
   init(project_directory)
-  writeLines('.Rproj.user',con = str_c(project_directory,'/.gitignore'))
+  writeLines(ignore,con = str_c(project_directory,'/.gitignore'))
   add(project_directory,'.*')
   add(project_directory,'*')
   commit(project_directory,all = TRUE,message = 'Initial commit')
