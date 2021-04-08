f1654ced0e378bc7b6324beced683ae974fe809a
jasenfinch <jsf9@aber.ac.uk>
2021-04-09 00:40:31 +0100

    renv infrastructure now exported

--- R/renv.R
+++ R/renv.R
@@ -1,6 +1,14 @@
+#' Initialise renv infrastructure
+#' @description Initialise renv infrastructure in a project directory.
+#' @param project_directory the project directory file path
+#' @param rebuild Force packages to be rebuilt, thereby bypassing any installed versions of the package available in the cache? This can either be a boolean (indicating that all installed packages should be rebuilt), or a vector of package names indicating which packages should be rebuilt.
+#' @examples 
+#' projectSkeleton('test project',path = tempdir(),type = 'report')
+#' renvInitialise(paste0(tempdir(),'/test_project'))
 #' @importFrom renv hydrate snapshot activate restore
 #' @importFrom callr r
 #' @importFrom usethis proj_set
+#' @export
 
 renvInitialise <- function(project_directory,rebuild = FALSE){
   message('Initialising renv cache')
