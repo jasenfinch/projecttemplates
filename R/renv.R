bba3664857de7466cc4dabf97b84c53d84c036f4
jasenfinch <jsf9@aber.ac.uk>
2021-04-30 21:08:38 +0100

    removed rebuild argument

--- R/renv.R
+++ R/renv.R
@@ -3,7 +3,6 @@
 #' @param project_directory the project directory file path
 #' @param bioc bioconductor packages to install into the cache upon initialisation
 #' @param github GitHub packages to install into the cache upon initialisation
-#' @param rebuild Force packages to be rebuilt, thereby bypassing any installed versions of the package available in the cache? This can either be a boolean (indicating that all installed packages should be rebuilt), or a vector of package names indicating which packages should be rebuilt.
 #' @examples 
 #' \dontrun{
 #' projectSkeleton(paste0(tempdir(),'/test_project'))
@@ -15,8 +14,7 @@
 
 renvInitialise <- function(project_directory,
                            bioc = character(),
-                           github = character(),
-                           rebuild = FALSE){
+                           github = character()){
   message('Initialising renv cache')
   renv_init <- r(function(project_directory,bioc,github){
     
