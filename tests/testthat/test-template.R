9e938faf4ec268ca34ed7c9ddf40851ec2e594da
jasenfinch <jsf9@aber.ac.uk>
2021-06-07 14:10:21 +0100

    test fix

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -5,9 +5,11 @@ test_that('report template works',{
   
   temp_path <- tempdir()
   
-  template('report test',path = temp_path,
+  template('report test',
+           path = temp_path,
            type = 'report',
            renv = FALSE,
+           git = FALSE,
            github = FALSE,
            start = FALSE)
   
@@ -24,6 +26,7 @@ test_that('presentation template works',{
            path = temp_path,
            type = 'presentation',
            renv = FALSE,
+           git = FALSE,
            github = FALSE,
            start = FALSE)
   
@@ -40,6 +43,7 @@ test_that('manuscript template works',{
            path = temp_path,
            type = 'manuscript',
            renv = FALSE,
+           git = FALSE,
            github = FALSE,
            start = FALSE)
   
