fc5fb8367ac358182ddb68e3c82a856da2303c3a
jasenfinch <jsf9@aber.ac.uk>
2022-05-31 00:27:01 +0100

    use renv::init bioconductor argument

--- R/renv.R
+++ R/renv.R
@@ -1,7 +1,6 @@
 #' Initialise renv infrastructure
 #' @description Initialise renv infrastructure in a project directory.
 #' @param project_directory the project directory file path
-#' @param bioc bioconductor packages to install into the cache upon initialisation
 #' @param github GitHub packages to install into the cache upon initialisation
 #' @examples 
 #' \dontrun{
@@ -13,24 +12,19 @@
 #' @export
 
 renvInitialise <- function(project_directory,
-                           bioc = character(),
                            github = character()){
   message('Initialising renv cache')
   renv_init <- r_copycat(function(project_directory,bioc,github){
     
     project_directory <- normalizePath(project_directory)
     
-    renv::init(project = project_directory,bare = TRUE)
-    
-    if (length(bioc) > 0) {
-      renv::install(paste0('bioc::',bioc),project = project_directory)
-    }
+    bioconductor <- ifelse(isTrue(bioc),TRUE,FALSE)
+    renv::init(project = project_directory,
+               bioconductor = bioconductor)
     
     if (length(github) > 0) {
       renv::install(github,project = project_directory)  
     }
-    
-    renv::hydrate(project = project_directory)
     
     renv::snapshot(project = project_directory)
     
