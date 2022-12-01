3929c67bdaa2f8dbf3230006b31cdfcd27800f1a
jasenfinch <jsf9@aber.ac.uk>
2022-12-01 15:07:42 +0000

    fix missing bioconductor argument in renvInitialise and add missing renv::hydrate command to add missing dependencies after initialisation

--- R/renv.R
+++ R/renv.R
@@ -1,6 +1,7 @@
 #' Initialise renv infrastructure
 #' @description Initialise renv infrastructure in a project directory.
 #' @param project_directory the project directory file path
+#' @param bioconductor The version of bioconductor to use with the project. Set to TRUE to use the default version of Bioconductor.
 #' @param github GitHub packages to install into the cache upon initialisation
 #' @examples 
 #' \dontrun{
@@ -12,24 +13,28 @@
 #' @export
 
 renvInitialise <- function(project_directory,
+                           bioconductor = FALSE,
                            github = character()){
   message('Initialising renv cache')
-  renv_init <- r_copycat(function(project_directory,bioc,github){
+  renv_init <- r_copycat(function(project_directory,bioconductor,github){
     
     project_directory <- normalizePath(project_directory)
     
-    bioconductor <- ifelse(isTrue(bioc),TRUE,FALSE)
     renv::init(project = project_directory,
-               bioconductor = bioconductor)
+               bioconductor = bioconductor,
+               bare = TRUE)
     
     if (length(github) > 0) {
       renv::install(github,project = project_directory)  
     }
     
-    renv::snapshot(project = project_directory)
+    renv::hydrate(project = project_directory)
+    
+    renv::snapshot(project = project_directory,
+                   prompt = FALSE)
     
   },
   args = list(project_directory = project_directory,
-              bioc = bioc,
+              bioconductor = bioconductor,
               github = github))
 }
